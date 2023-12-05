a2026f9d9ce31896b286da260b180fedb8b22303
Passley <passley.pascal@gmail.com>
2023-12-05 23:23:31 +0100

    q

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,15 +47,11 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-base:18.2.0")
     implementation("androidx.annotation:annotation:1.7.0")
-    implementation ("org.jetbrains.kotlin:kotlin-gradle-plugin:")
-    implementation ("androidx.navigation:navigation-safe-args-gradle-plugin:")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.navigation:navigation-fragment:2.7.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("com.google.android.gms:play-services-auth:20.7.0")
 
 
     
