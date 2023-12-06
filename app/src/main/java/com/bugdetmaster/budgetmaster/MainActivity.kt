17740640bfafff4ee74aea42a76c1fe69a4a2153
Passley <passley.pascal@gmail.com>
2023-12-06 16:38:08 +0100

    Hopefully BottomNavigation implemented

--- app/src/main/java/com/bugdetmaster/budgetmaster/MainActivity.kt
+++ app/src/main/java/com/bugdetmaster/budgetmaster/MainActivity.kt
@@ -7,6 +7,8 @@ import android.view.MenuItem
 import android.widget.FrameLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -15,19 +17,26 @@ import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+/**
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setupActionBarWithNavController(navController)
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
+
+*/
+
 /**
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
         val navController = findViewById<FrameLayout>(R.id.nav_host_fragment).findNavController()
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.uebersichtFragment, R.id.ausgabenFragment, R.id.sparplanFragment, R.id.settingsOneFragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-
-        bottomNavigationView.setupWithNavController(navController)
-
-
 */
     }
 
--- app/src/main/java/com/bugdetmaster/budgetmaster/SettingsOneFragment.kt
+++ app/src/main/java/com/bugdetmaster/budgetmaster/SettingsOneFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.switchmaterial.SwitchMaterial
 
 
@@ -13,6 +14,33 @@ class SettingsOneFragment : Fragment(R.layout.fragment_settings_one) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<SwitchMaterial>(R.id.switch_LightDark).setOnClickListener {  }
+        lightDarkMode(view.findViewById<SwitchMaterial>(R.id.switch_LightDark))
+
+
+        view.findViewById<SwitchMaterial>(R.id.switch_LightDark).setOnClickListener {
+
+           /** if(view.findViewById<SwitchMaterial>(R.id.switch_LightDark).isEnabled){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    //Text auf ENABLE DARKMODE
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                //Text auf DISABLE DARKMODE
+            }
+
+                */
+        }
+    }
+
+    private fun lightDarkMode(switchMaterial: SwitchMaterial){
+
+       /** if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO){
+            boolean = false
+            //Text auf ENABLE DARKMODE
+        } else if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            boolean = true
+            //Text auf DISABLE DARKMODE
+        }else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } */
     }
 }
