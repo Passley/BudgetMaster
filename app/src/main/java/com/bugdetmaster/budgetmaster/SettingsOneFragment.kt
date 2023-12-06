943e92b9a90868ef26a44d78d08ed2c8f710adf1
Passley <passley.pascal@gmail.com>
2023-12-06 16:29:50 +0100

    changed Directions

--- app/src/main/java/com/bugdetmaster/budgetmaster/SettingsOneFragment.kt
+++ app/src/main/java/com/bugdetmaster/budgetmaster/SettingsOneFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 
 class SettingsOneFragment : Fragment(R.layout.fragment_settings_one) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<SwitchMaterial>(R.id.switch_LightDark).setOnClickListener {  }
+    }
 }
