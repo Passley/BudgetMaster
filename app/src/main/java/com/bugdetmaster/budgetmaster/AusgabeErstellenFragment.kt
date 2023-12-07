e96eb2b602ed9223d01ab0ecb6f6626b24e15595
Vkarr <151539041+Vkarr@users.noreply.github.com>
2023-12-07 17:12:19 +0100

    hinzugefügt

---
+++ app/src/main/java/com/bugdetmaster/budgetmaster/AusgabeErstellenFragment.kt
@@ -0,0 +1,11 @@
+package com.bugdetmaster.budgetmaster
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AusgabeErstellenFragment : Fragment(R.layout.fragement_ausgaben_erstellen) {
+
+}
