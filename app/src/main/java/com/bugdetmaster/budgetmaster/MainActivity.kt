15bf41431690c7b3165e4c64c690c91ad457c94e
finnbingenheimer <finn.bingenheimer@gmail.com>
2023-12-11 17:19:57 +0100

    alles

--- app/src/main/java/com/bugdetmaster/budgetmaster/MainActivity.kt
+++ app/src/main/java/com/bugdetmaster/budgetmaster/MainActivity.kt
@@ -26,7 +26,6 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
 
-        setupActionBarWithNavController(navController)
 
         findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
 
