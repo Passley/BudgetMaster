81fb73421274d0b75bf2bf96e33582c663b4f93f
Passley <passley.pascal@gmail.com>
2023-12-17 23:57:43 +0100

    Implemented readTransaction but NOT working

--- app/src/main/java/com/bugdetmaster/budgetmaster/AusgabenFragment.kt
+++ app/src/main/java/com/bugdetmaster/budgetmaster/AusgabenFragment.kt
@@ -1,6 +1,7 @@
 package com.bugdetmaster.budgetmaster
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
@@ -8,8 +9,28 @@ import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.navigation.fragment.findNavController
+import com.bugdetmaster.budgetmaster.data.RetrofitApi
+import com.bugdetmaster.budgetmaster.data.login.Login
+import com.bugdetmaster.budgetmaster.data.login.LoginResponse
+import com.bugdetmaster.budgetmaster.data.transaction.readTransaction
+import com.bugdetmaster.budgetmaster.data.transaction.readTransactionResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.math.log
 
 class AusgabenFragment : Fragment(R.layout.fragment_ausgaben) {
+    //Tag für die Konsole
+    final val TAG = "BUDGETMASTER"
+
+    //Die URL des Servers
+    val BASE_URL = "http://85.215.77.230/"
+
+    companion object{
+        lateinit var Api: Retrofit
+    }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -21,7 +42,9 @@ class AusgabenFragment : Fragment(R.layout.fragment_ausgaben) {
             //val action = AusgabenFragmentDirections.actionAusgabenFragmentToAusgabeErstellenFragment()
             //findNavController().navigate(action)
         }
-
+        Thread{
+            readTransaction()
+        }
 
 
         /**
@@ -35,5 +58,62 @@ class AusgabenFragment : Fragment(R.layout.fragment_ausgaben) {
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object: OnBackPressedCallback(true){
             override fun handleOnBackPressed() {}
         })
+    }
+
+    fun readTransaction(){
+        val api = login()
+
+
+        val data2: readTransaction = com.bugdetmaster.budgetmaster.data.transaction.readTransaction(48)
+
+        api.ReadTransaction(data2).enqueue(object : Callback<readTransactionResponse>{
+            override fun onResponse(
+                call: Call<readTransactionResponse>,
+                response: Response<readTransactionResponse>
+            ) {
+                if (response.isSuccessful){
+                    response.body()?.let {
+                        Log.i(TAG, "${it.amount}, ${it.transaction_id}, ${it.name}")
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<readTransactionResponse>, t: Throwable) {
+                Log.e(TAG, "${t.message}")
+            }
+        })
+    }
+
+    fun login(): RetrofitApi {
+        //Client
+        val api = initRetro2()
+
+        val password = "1234"
+        val username = "AndroidPass"
+        //Erstellen des Datenobjekts
+        val data: Login = Login(username, password)
+
+        api.setLogin(data).enqueue(object : Callback<LoginResponse> {
+
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                if (response.isSuccessful){
+                    Log.i(TAG, "Erfolgreich angemeldet...")
+                }
+            }
+
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                Log.e(TAG, "Response Login: ${t.message}")
+            }
+        })
+        return api
+    }
+
+    fun initRetro2(): RetrofitApi {
+        val api = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RetrofitApi::class.java)
+        return api
     }
 }
